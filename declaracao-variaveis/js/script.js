a28aa82017170b46681dee0b1f0c3dba12ecd3f8
Matheus-Bueno <matheusdbuenoj@outlook.com>
2025-01-20 17:30:12 -0300

    Aula 2

---
+++ declaracao-variaveis/js/script.js
@@ -0,0 +1,24 @@
+let nome = "João"
+console.log(nome)
+
+nome = 100
+console.log(nome)
+
+var nomeCompleto = "João da Silva"
+console.log(nomeCompleto)
+
+nomeCompleto = 200
+console.log(nomeCompleto)
+
+const elemento = "algumTexto"
+console.log(elemento)
+
+//  Demonstração de erro ao tentar altera uma constante
+// elemento = 300
+// console.log(elemento)
+
+const dolarParalelo = 5.45
+console.log("DOLAR " + dolarParalelo)
+
+const diaSemana = ["SEG", "TER", "QUA", "QUI", "SEX", "SAB", "DOM"]
+console.log("DIA DA SEMANA " + diaSemana[0] )
